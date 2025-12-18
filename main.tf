27e740e6e500b77364eb25e32cf7cbf21f6220e6
Sai Zayar Tun <151984847+Sine22@users.noreply.github.com>
2025-12-18 09:01:11 +0700

    Configure AWS key pair and security group
    Added AWS resources for key pair and security group.

--- main.tf
+++ main.tf
@@ -1,21 +1,54 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+provider "aws" {
+  region = "ap-southeast-1"
+}
+
+resource "aws_key_pair" "main" {
+  key_name   = "mykey"
+  ## replace the key with the content from previous command
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPtuDB8dQm10ZkAsKoi7/kG3XwaBgU4/Xb7Zp1FRtW/I laborant@flexbox (managed)"
+}
+
+resource "aws_security_group" "main" {
+  name        = "allow-ssh-and-4444"
+  description = "Allow SSH and port 4444 from anywhere"
+
+  ingress {
+    description = "SSH from anywhere"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Port 4444 from anywhere"
+    from_port   = 4444
+    to_port     = 4444
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow-ssh-and-4444"
   }
 }
 
-provider "aws" {
-  region = "ap-southeast-1" # change if needed
-}
-
-resource "aws_instance" "example" {
-  ami           = "ami-0df7a207adb9748c7" # Ubuntu 22.04 (Singapore)
+resource "aws_instance" "main" {
+  ami           = "ami-00d8fc944fb171e29"
   instance_type = "t3.micro"
+  key_name      = aws_key_pair.main.id
+  
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
-    Name = "terraform-ec2"
+    Name = "HelloWorld"
   }
 }
